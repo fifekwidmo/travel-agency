830cd0c72b50bc964044a1d5cd7752f888c2e107
Filip Cwiek <fifek.inex@gmail.com>
2020-05-30 18:38:30 +0200

    Add links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-// TODO - import other views
+// TODO - import other views [DONE]
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -41,6 +45,10 @@ class App extends React.Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/trip/:id' component={Trip} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
